10ffd67ce787712a05c440a6f59b927c2eac1d12
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-07-31 11:53:44 -0500

    Added Comments

--- src/version.hpp
+++ src/version.hpp
@@ -1,3 +1,11 @@
+/*
+ * file: version.hpp
+ * Program: Source Search Version (C++)
+ *
+ * Writer: Daniel McGuire
+ *
+ * Purpose: Just to keep the versionstring seperate.
+ */
 #ifndef SSVERSION_H
 #define SSVERSION_H
 
