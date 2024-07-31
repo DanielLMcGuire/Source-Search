db6f516ed68c3265424c03a1a10a05e54d57300f
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-07-31 11:52:15 -0500

    Added Comments

--- src/extensions.hpp
+++ src/extensions.hpp
@@ -1,10 +1,18 @@
+/*
+ * file: extensions.hpp
+ * Header: Extensions for Source Search (C++)
+ *
+ * Writer: Daniel McGuire
+ *
+ * Purpose: Provides Source Search with File Extensions.
+ */
 #ifndef SOURCEEXTENSIONS_H
 #define SOURCEEXTENSIONS_H
 
 #include <vector>
 #include <string>
 
-const std::vector<std::string> sourceCodeExtensions = {
+const std::vector<std::string> sourceCodeExtensions = { // Below are the extensions:
     "c", "cpp", "h", "hpp", "cc", "cxx", "hxx", // C and C++
     "java", "class", // Java
     "py", "pyc",  // Python
