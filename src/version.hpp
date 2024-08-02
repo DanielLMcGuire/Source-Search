a8474d4b2d9299dce38c8cb76e24c45c183230d9
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-08-02 15:24:11 -0700

    Typo

--- src/version.hpp
+++ src/version.hpp
@@ -4,7 +4,7 @@
  *
  * Writer: Daniel McGuire
  *
- * Purpose: Just to keep the versionstring seperate.
+ * Purpose: Just to keep the version string seperate.
  */
 #ifndef SSVERSION_H
 #define SSVERSION_H
