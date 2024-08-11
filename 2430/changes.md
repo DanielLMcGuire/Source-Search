d39f0e083a92e7ff8ae799b29a0c05d5a3b5343f
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-08-10 23:02:16 -0500

    Added v1.0.0

---
+++ 2430/changes.md
@@ -0,0 +1,20 @@
+First Release
+
+
+## How to install:
+
+Debian:
+```bash
+sudo apt install ./sourcesearch_1.0.0.deb
+```
+Arch:
+```bash
+sudo pacman -U ./sourcesearch-1.0.0-any.pkg.tar.zst
+```
+
+AppImage:
+
+```bash
+chmod +x ./sourcesearch-1.0.0-x86_64.AppImage
+./sourcesearch-1.0.0-x86_64.AppImage
+```
---
+++ 2430/download.js
@@ -0,0 +1,48 @@
+function showLinuxPopup() {
+    document.getElementById("linuxPopup").style.display = "block";
+}
+
+function closeLinuxPopup() {
+    document.getElementById("linuxPopup").style.display = "none";
+}
+
+function showComingSoon() {
+    document.getElementById("comingSoonPopup").style.display = "block";
+}
+
+function closeComingSoonPopup() {
+    document.getElementById("comingSoonPopup").style.display = "none";
+}
+
+function sourceCode() {
+    window.location.href = "https://github.com/DanielLMcGuire/Source-Search/archive/refs/tags/1.0.0.zip";
+}
+
+
+function downloadWindows() {
+    // Redirect to the Windows download link
+    window.location.href = "https://github.com/DanielLMcGuire/Source-Search/releases/download/1.0.0/sourcesearch_installer1.0.0.exe";
+}
+
+function loadReleaseNotes() {
+    fetch('changes.md')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(text => {
+            const converter = new showdown.Converter();
+            document.getElementById('release-notes').innerHTML = converter.makeHtml(text);
+        })
+        .catch(error => {
+            console.error('Error loading release notes:', error);
+            document.getElementById('release-notes').innerHTML = '<p>Error loading release notes.</p>';
+        });
+}
+
+// Load release notes on page load
+window.onload = function() {
+    loadReleaseNotes();
+};
